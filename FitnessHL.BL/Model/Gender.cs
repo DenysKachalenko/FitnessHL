6a9937b3f4196366de18242e35a75cc6ffe3ec6c
Denis Kachalenko <kachalenko.denis@gmail.com>
2019-12-24 21:24:15 +0200

    Створена модель Користувача. Create model User.

---
+++ FitnessHL.BL/Model/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FitnessHL.BL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Create new gender.
+        /// </summary>
+        /// <param name="name"> Name gender </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Null or empty!", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FitnessHL.BL/Model/User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessHL.BL.Model
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class User
+    {
+        #region Properties
+        /// <summary>
+        /// Name user.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gender user.
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// Birth date user.
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Weight user.
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Growth user.
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name"> Name user. </param>
+        /// <param name="gender"> Gender user. </param>
+        /// <param name="birthDate"> Birth date user. </param>
+        /// <param name="weight"> Weight user. </param>
+        /// <param name="height"> Growth user. </param>
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            #region Checking conditions
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name null or empty!", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Gender cannot be null!", nameof(gender));
+            }
+
+            if (birthDate < DateTime.Parse("01.01.1930") || birthDate <= DateTime.Now)
+            {
+                throw new ArgumentException("Birth date is impossible!", nameof(birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("The weight cannot be less than zero or equal to zero!", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("The growth cannot be less than zero or equal to zero!", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
